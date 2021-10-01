75b062c73fa8f03e28da8074d9e48bf69e146571
huzefa51 <huzefa.hamdard53@gmail.com>
2021-10-01 19:00:50 +0900

    Update handler.py

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ import datetime
 def endpoint(event, context):
     current_time = datetime.datetime.now().time()
     body = {
-        "message": "Hello, the current time is " + str(current_time)
+        "message": "Hello Huzefa, the current time is " + str(current_time)
     }
 
     response = {
